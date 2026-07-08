a59c6f8ae06942df40cbc09662c41e2d1368880b
denielson2099 <denielsonlima201099@gmail.com>
2026-07-07 21:36:47 -0300

    Corrige validacao UUID no checkout EAD

--- supabase/functions/gateways/checkout/utils.ts
+++ supabase/functions/gateways/checkout/utils.ts
@@ -5,7 +5,7 @@ import type {
 } from "./types.ts";
 
 export const UUID_RE =
-  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i;
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 export const PENDENTE_INSCRICAO_STATUS = "AGUARDANDO_PAGAMENTO";
 
