1b33d0d3fd2e7ebb803f678266e7e918f06af5f4
DenielsonSLima <denielsonlima201099@gmail.com>
2026-07-25 11:24:56 -0300

    fix(ead): remove parcelamento do cadastro

--- modules/gestor/cadastros/cadastros.service.ts
+++ modules/gestor/cadastros/cadastros.service.ts
@@ -43,7 +43,7 @@ export const DEFAULT_EAD_FINANCEIRO_CONFIG: CursoFinanceiroConfig = {
   },
   cartao: {
     aceitar: true,
-    maxParcelas: 2,
+    maxParcelas: 1,
     aplicarDescontoPontualidade: false,
     repassarCustoParcelamento: false
   },
