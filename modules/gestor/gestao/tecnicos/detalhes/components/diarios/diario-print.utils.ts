6c047be18dbb853b0045b45b5a0bdd2ae9246e59
DenielsonSLima <denielsonlima201099@gmail.com>
2026-07-26 11:09:50 -0300

    feat(academico): prepara diários técnicos beta 0.7.0

--- modules/gestor/gestao/tecnicos/detalhes/components/diarios/diario-print.utils.ts
+++ modules/gestor/gestao/tecnicos/detalhes/components/diarios/diario-print.utils.ts
@@ -7,6 +7,18 @@ export const chunks = <T,>(items: T[], size: number): T[][] => {
   return result;
 };
 
+export const DIARIO_RESULT_LEGEND_TITLE = 'LEGENDA: Instrumentos Avaliativos:';
+
+export const DIARIO_RESULT_LEGEND_TEXT = [
+  'P - Prova',
+  'TI - Trabalho Individual',
+  'TG - Trabalho em Grupo',
+  'S - Seminários',
+  'CQ - Critérios Qualitativos (assiduidade, pontualidade, responsabilidade, participação = 1 ponto)',
+  'O - Outros Instrumentos',
+  'Resultado Final: APROVADO/REPROVADO',
+].join(' / ');
+
 export const moduloNumero = (nome: string) => {
   const match = nome.match(/M[ÓO]DULO\s+([IVXLC]+)/i);
   return match?.[1] || nome;
