264a2976626ee5fda1af7f3b119ae2d13dc5a070
DenielsonSLima <denielsonlima201099@gmail.com>
2026-07-31 10:37:20 -0300

    beta.18: atualiza modules/gestor/configuracoes/polos/PolosConfig.tsx

--- modules/gestor/configuracoes/polos/PolosConfig.tsx
+++ modules/gestor/configuracoes/polos/PolosConfig.tsx
@@ -14,6 +14,7 @@ import {
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { polosService, Polo } from './polos.service';
 import { supabase } from '../../../../lib/supabase';
+import { usuariosKeys } from '../usuarios/usuarios.keys';
 
 // Novos componentes de modal customizados
 import ModalExcluirPolo from './components/ModalExcluirPolo';
@@ -92,6 +93,7 @@ const PolosConfig: React.FC = () => {
         () => {
           console.log('Alteração detectada em polos, recarregando...');
           queryClient.invalidateQueries({ queryKey: ['polos'] });
+          queryClient.invalidateQueries({ queryKey: usuariosKeys.polos() });
         }
       )
       .subscribe();
@@ -113,6 +115,7 @@ const PolosConfig: React.FC = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['polos'] });
       queryClient.invalidateQueries({ queryKey: ['active_polos'] });
+      queryClient.invalidateQueries({ queryKey: usuariosKeys.polos() });
       setIsEditing(null);
       setEditingPolo({});
     },
@@ -124,6 +127,7 @@ const PolosConfig: React.FC = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['polos'] });
       queryClient.invalidateQueries({ queryKey: ['active_polos'] });
+      queryClient.invalidateQueries({ queryKey: usuariosKeys.polos() });
       if (isEditing) {
         setIsEditing(null);
         setEditingPolo({});
