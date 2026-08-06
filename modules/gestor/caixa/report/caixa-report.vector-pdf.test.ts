e3109781c077fd7977d68ca52ccf0e04d26967bb
DenielsonSLima <denielsonlima201099@gmail.com>
2026-08-06 01:19:37 -0300

    test(caixa): importa Buffer explicitamente

--- modules/gestor/caixa/report/caixa-report.vector-pdf.test.ts
+++ modules/gestor/caixa/report/caixa-report.vector-pdf.test.ts
@@ -1,4 +1,5 @@
 import assert from 'node:assert/strict';
+import { Buffer } from 'node:buffer';
 import { readFile, writeFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import test from 'node:test';
