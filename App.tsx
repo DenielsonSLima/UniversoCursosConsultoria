873ef5ab61209e4b9db2365ad2cf375ef92af198
DenielsonSLima <denielsonlima201099@gmail.com>
2026-08-20 18:03:34 -0300

    fix(release): remove portal coordenador sem base de autenticação

--- App.tsx
+++ App.tsx
@@ -44,7 +44,6 @@ const PasswordRecoveryPage = lazy(() => import('./modules/login/PasswordRecovery
 const LoginPage = lazy(() => import('./modules/login/LoginPage'));
 const GestorPage = lazy(() => import('./modules/gestor/gestor.page'));
 const ProfessorPage = lazy(() => import('./modules/professor/professor.page'));
-const CoordenadorPage = lazy(() => import('./modules/coordenador/coordenador.page'));
 const AlunoPage = lazy(() => import('./modules/aluno/aluno.page'));
 const TechnicalLandingRoute = lazy(() => import('./modules/public/landing-pages/cursos-tecnicos/TechnicalLandingRoute'));
 const BioPage = lazy(() => import('./modules/public/bio/BioPage'));
@@ -52,7 +51,6 @@ const RouteLoadingScreen = () => {
   const pathname = window.location.pathname;
   if (pathname.startsWith('/gestor')) return <AccessCheckingScreen portal="Gestor" />;
   if (pathname.startsWith('/professor')) return <AccessCheckingScreen portal="Professor" />;
-  if (pathname.startsWith('/coordenador')) return <AccessCheckingScreen portal="Coordenador" />;
   if (pathname === '/aluno' || pathname.startsWith('/aluno/')) return <AlunoAppSplash />;
 
   return (
@@ -148,7 +146,6 @@ const App: React.FC = () => {
         <Route path="/sistema/login" element={<LoginPage />} />
         <Route path="/gestor/*" element={<VersionedPortal><GestorPage /></VersionedPortal>} />
         <Route path="/professor/*" element={<VersionedPortal><ProfessorPage /></VersionedPortal>} />
-        <Route path="/coordenador/*" element={<VersionedPortal><CoordenadorPage /></VersionedPortal>} />
         <Route path="/cad-aed" element={<Navigate to="/sistema/login" replace />} />
         <Route path="/aluno/*" element={<VersionedPortal><AlunoPage /></VersionedPortal>} />
 
